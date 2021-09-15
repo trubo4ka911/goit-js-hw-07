73919a69c83566423311471c3339f76f2e5d00ec
Anna Trubnikova <trubo4ka911@gmail.com>
2021-09-15 11:57:49 +0300

    Update task-01.js
    2021

--- js/task-01.js
+++ js/task-01.js
@@ -1,42 +1,19 @@
-(() => {
-	const selector = "categories";
-	const categoriesEl = document.querySelector(`#${selector}`);
-	if (!categoriesEl) {
-		console.log(`Selector "#${selector}" not find`);
-		return;
-	}
-	const countCategories = function (cat) {
-		const endings1 = [11, 12, 12, 14];
-		const endings2 = [2, 3, 4];
-		let result = "";
-		if (endings1.includes(Number.parseInt(cat.childElementCount.toString().slice(-2)))) {
-			result = `В списке ${cat.childElementCount} категорий.`;
+const navEl = document.querySelector('#categories');
+const NavItemEl = navEl.childElementCount;
+console.log(`В списке ${NavItemEl} категории.`)
 
-			return result;
-		} else if (endings2.includes(Number.parseInt(cat.childElementCount.toString().slice(-1)))) {
-			result = `В списке ${cat.childElementCount} категории.`;
 
-			return result;
-		} else if (Number.parseInt(cat.childElementCount.toString().slice(-1)) === 1) {
-			result = `В списке ${cat.childElementCount} категория.`;
+const items = document.querySelectorAll('.item');
+Array.prototype.forEach.call(items, (element) => {
+    const title = element.querySelector('h2').innerHTML;
+    const itemsLength = element.querySelectorAll('li').length;
+    console.log(`Категория: ${title}\nКоличество элементов: ${itemsLength}`);
+});
 
-			return result;
-		} else {
-			result = `В списке ${cat.childElementCount} категорий.`;
 
-			return result;
-		}
-	};
-	console.log(countCategories(categoriesEl));
-	for (let i = 0; i < categoriesEl.childElementCount; i += 1) {
-		for (let j = 0; j < categoriesEl.children[i].childElementCount; j += 1) {
-			if (categoriesEl.children[i].children[j].nodeName === "H2") {
-				console.log("Категория: ", categoriesEl.children[i].children[j].textContent);
-				continue;
-			}
-			if (categoriesEl.children[i].children[j].nodeName === "UL") {
-				console.log("Количество элементов: ", categoriesEl.children[i].children[j].childElementCount);
-			}
-		}
-	}
-})();
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
