1d6aab7ebdfbbd5e9d87f6fe7b19b556270e00e2
Anna Trubnikova <trubo4ka911@gmail.com>
2021-09-18 16:31:59 +0300

    2021

--- js/task-03.js
+++ js/task-03.js
@@ -1,34 +1,20 @@
-(() => {
-	const selector = "gallery";
-	const galleryEl = document.querySelector(`#${selector}`);
-	if (!galleryEl) {
-		console.log(`Selector "#${selector}" not find`);
-		return;
-	}
-	const images = [
-		{
-			url:
-				"https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-			alt: "White and Black Long Fur Cat",
-		},
-		{
-			url:
-				"https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-			alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-		},
-		{
-			url:
-				"https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-			alt: "Group of Horses Running",
-		},
-	];
-	galleryEl.classList.add(`${selector}`);
-	galleryEl.insertAdjacentHTML(
-		"afterbegin",
-		images.reduce((acc, { url, alt }) => {
-			acc += `<li class="${selector}__item"><img class="${selector}__image" src="${url}" alt="${alt}"/></li>`;
+const images = [
+    {
+      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
+  
+const ulList = document.getElementById('gallery')
 
-			return acc;
-		}, ""),
-	);
-})();
+const markup = images.map(image => `<li><img src="${image.url}"></li>`).join('');
+
+ulList.insertAdjacentHTML("beforeend", markup)
