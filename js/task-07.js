b16529e6b281d2ed50694888c9ec5a29150b5cfc
Anna Trubnikova <trubo4ka911@gmail.com>
2021-09-19 16:06:12 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,7 @@
-(() => {
-	const selectorFont = "font-size-control";
-	const selectorText = "text";
-	const textEl = document.querySelector(`#${selectorText}`);
-	const fontSizeControlEl = document.querySelector(`#${selectorFont}`);
-	if (!fontSizeControlEl || !textEl) {
-		console.log(`Selector "#${fontSizeControlEl}" or "#${textEl}" not find`);
-	}
-	textEl.style.fontSize = `${fontSizeControlEl.value}px`;
-	fontSizeControlEl.addEventListener("input", () => {
-		textEl.style.fontSize = `${fontSizeControlEl.value}px`;
+const selectorFont = document.getElementById('font-size-control');
+	const selectorText = document.getElementById('text');
+	
+	selectorText.style.fontSize = `${selectorFont.value}px`;
+	selectorFont.addEventListener("input", () => {
+		selectorText.style.fontSize = `${selectorFont.value}px`;
 	});
-})();
